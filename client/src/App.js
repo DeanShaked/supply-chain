bb77becb89679952919ac8246fdb922906f298c1
Dean Shaked <deanshak9@gmail.com>
2022-03-14 20:02:46 +0200

    added unit test

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
+      this.listenToPaymentEvent();
       this.setState({ loaded: true });
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -41,6 +42,18 @@ class App extends Component {
       );
       console.error(error);
     }
+  };
+
+  listenToPaymentEvent = () => {
+    let self = this;
+    this.itemManager.events.SupplychainStep().on("data", async function (evt) {
+      console.log("evt :>> ", evt);
+      let itemObj = await self.itemManager.methods
+        .items(evt.returnValues._itemIndex)
+        .call();
+
+      alert(`Item ${itemObj._identifier} was delivered.`);
+    });
   };
 
   handleInputChange = (event) => {
---
+++ test/ItemManagerTest.js
@@ -0,0 +1,19 @@
+const ItemManager = artifacts.require("./ItemManager.sol");
+
+contract("ItemManager", (accounts) => {
+  it(" Shoud be able to add an item", async function () {
+    const itemManagerInstance = await ItemManager.deployed();
+    const itemName = "test1";
+    const itemPrice = 500;
+
+    const result = await itemManagerInstance.createItem(itemName, itemPrice, {
+      from: accounts[0],
+    });
+
+    assert.equal(result.logs[0].args._itemIndex, 0, "Its not the first time.");
+
+    const item = await itemManagerInstance.items(0);
+
+    assert.equal(item._identifier, itemName, "The identifier was different.");
+  });
+});
